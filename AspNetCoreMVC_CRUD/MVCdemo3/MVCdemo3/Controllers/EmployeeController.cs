9954ac8b7a5e72dcfc94cb029ec019b4d04d5a53
ShubhamDev27 <shubhamchopdar27@gmail.com>
2025-04-23 00:03:56 +0530

    first commit

---
+++ AspNetCoreMVC_CRUD/MVCdemo3/MVCdemo3/Controllers/EmployeeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCdemo3.Models;
+using MVCdemo3.Service;
+
+namespace MVCdemo3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployee emp;
+        public EmployeeController(IEmployee emp)
+        {
+            this.emp = emp; 
+        }
+
+        public IActionResult Index()
+        {
+            var model = emp.GetAllEmployees();
+
+            return View(model);
+        }
+        public IActionResult Details(int id)
+        {
+            var model = emp.GetEmployee(id);
+            return View(model);
+        }
+        public IActionResult Edit(int id)
+        {
+            var model = emp.GetEmployee(id);
+
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Employee emp1)
+        {
+            if(ModelState.IsValid)
+            {
+                var model = emp.Update(emp1);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            var model = emp.GetEmployee(id);
+
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeleteData(int id)
+        {
+            try
+            {
+
+                emp.Delete(id);
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch 
+            {
+                return View();
+                    
+            }
+        }
+
+        public IActionResult Create(Employee emp2)
+        {
+            var model = emp.Add(emp2);
+            return View(model);
+        }
+
+        //public IActionResult Create(Employee emp2)
+        //{
+        //       emp.Add(emp2);
+        //    return View(model);
+        //}
+
+    }
+}
---
+++ AspNetCoreMVC_CRUD/MVCdemo3/MVCdemo3/Service/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using MVCdemo3.Models;
+
+namespace MVCdemo3.Service
+{
+    public interface IEmployee
+    {
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployee(int id);
+        Employee Delete(int id);
+        Employee Update(Employee emp1);
+        Employee Add(Employee emp1);
+
+    }
+}
