7ccb4d88d26dd06662505ebc4b01be82728a53fe
ShubhamDev27 <shubhamchopdar27@gmail.com>
2025-05-06 21:26:59 +0530

    Entity FW

---
+++ EntityDemo4/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityDemo4.Models
+{
+    public class Book
+    {
+        [Key]
+       public int Id { get; set; }
+        [Required]
+       public string Title { get; set; }
+        [Required]
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }  
+    }
+}
---
+++ EntityDemo4/Repository/SampleContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityDemo4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityDemo4.Repository
+{
+    public class SampleContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectModels;Initial Catalog=ADOcode;Integrated Security=True;");
+
+        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Book>()
+        //        .HasOne(b => b.Author)
+        //        .WithMany(a => a.Books)
+        //        .HasForeignKey(b => b.AuthorId)
+        //        .OnDelete(DeleteBehavior.Restrict);
+        //}
+
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
+    
