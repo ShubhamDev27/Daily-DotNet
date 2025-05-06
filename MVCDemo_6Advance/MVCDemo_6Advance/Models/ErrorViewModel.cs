e3ad952f982f79aee037f3242a320c5a416d712b
ShubhamDev27 <shubhamchopdar27@gmail.com>
2025-05-06 21:22:46 +0530

    calculator

--- MVCDemo_6Advance/MVCDemo_6Advance/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVCDemo_6Advance.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
