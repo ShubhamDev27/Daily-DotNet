aba2e2fc751a5c31f387eeac2b413e0403d51532
ShubhamDev27 <shubhamchopdar27@gmail.com>
2025-05-15 20:08:48 +0530

    ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace MyMathLib1
+{
+
+    struct demo2
+    {
+        public int x;
+        public int y;
+        public demo2(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+        public void display()
+        {
+            Console.WriteLine($"{ x},{ y}");
+        }
+    }
+    class Myclass
+    {
+        public void Main(string[] args)
+        {
+            demo2 d1 = new demo2();
+            d1.display();
+
+        }
+
+
+    }
+}
