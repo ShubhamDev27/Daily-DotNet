9bda2f70aa9c3b0a211138fd476c3311af877d48
ShubhamDev27 <shubhamchopdar27@gmail.com>
2025-08-25 21:00:30 +0530

    some exercise

---
+++ SessionDemo2/SessionDemo2/Controllers/SessionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SessionDemo2.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SessionDemo2/SessionDemo2/Models/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SessionDemo2.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
